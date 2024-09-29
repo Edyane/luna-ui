28d3016c3ce1b43d7cffed2b421d581604dc7a97
Edyane <edyanealves@gmail.com>
2024-09-29 20:08:21 -0300

    feat: adicionando paleta de cores

---
+++ packages/tokens/src/colors.ts
@@ -0,0 +1,26 @@
+export const colors = {
+  white: '#FFF',
+  black: '#000',
+
+  gray100: '#E1E1E6',
+  gray200: '#A9A9B2',
+  gray400: '#7C7C8A',
+  gray500: '#505059',
+  gray600: '#323238',
+  gray700: '#29292E',
+  gray800: '#202024',
+  gray900: '#121214',
+
+  
+  luna50: '#edf2ff',
+  luna100: '#dee8ff',
+  luna200: '#c3d2ff',
+  luna300: '#9eb5ff',
+  luna400: '#788cff',
+  luna500: '#5864fc',
+  luna600: '#4141f2',
+  luna700: '#2f2dd5',
+  luna800: '#2727ac',
+  luna900: '#272888',
+  luna950: '#18174f',
+}
