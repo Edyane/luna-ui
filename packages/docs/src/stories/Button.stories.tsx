72cedf83bef836eb71cf637aafd23de5535b3456
Edyane <edyanealves@gmail.com>
2024-10-07 21:15:22 -0300

    feat: passando parâmetros para os botões do tipo: danger, sucess, large e small

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -11,10 +11,38 @@ export default {
 
 } as Meta<ButtonProps>
 
-export const Primary: StoryObj<ButtonProps> = {}
+// export const Primary: StoryObj<ButtonProps> = {}
 
-export const Big: StoryObj<ButtonProps> = {
+export const Primary: StoryObj<ButtonProps> = {
     args: {
-        size: 'big',
+        type: 'primary',
+        children: 'Primary Button'
+    },
+};
+
+export const Danger: StoryObj<ButtonProps> = {
+    args: {
+        type: 'danger',
+        children: 'Danger Button'
+    },
+};
+export const Success: StoryObj<ButtonProps> = {
+    args: {
+        type: 'success',
+        children: 'Success Button'
+    },
+};
+
+export const Large: StoryObj<ButtonProps> = {
+    args: {
+        size: 'large',
+        children: 'Large Button'
+    },
+};
+
+export const Small: StoryObj<ButtonProps> = {
+    args: {
+        size: 'small',
+        children: 'Small Button'
     },
 };
