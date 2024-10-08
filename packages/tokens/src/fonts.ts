63fa1b04a1abcf74e731786beba68aa947bfb47a
Edyane <edyanealves@gmail.com>
2024-10-07 21:16:10 -0300

    feat: ajustando tipografia

--- packages/tokens/src/fonts.ts
+++ packages/tokens/src/fonts.ts
@@ -1,4 +1,7 @@
 export const fonts = {
-  default: 'Roboto, sans-serif',
+  // default: 'Roboto, sans-serif',
+  // default: 'Open Sans, sans-serif',
+  // default: 'Poppins, sans-serif',
+  default: 'Montserrat, sans-serif',
   code: 'monospace',
 }
