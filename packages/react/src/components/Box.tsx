658cbeb56297433729cc8c8ac01ea9adadbcb179
Edyane <edyanealves@gmail.com>
2024-10-26 15:21:21 -0300

    feat: adicionando ajustes ao estilo da box

--- packages/react/src/components/Box.tsx
+++ packages/react/src/components/Box.tsx
@@ -2,10 +2,11 @@ import { ComponentProps, ElementType } from 'react';
 import { styled } from '../styles';
 
 export const Box = styled('div', {
+    width: '620px',
     padding: '$6',
     backgroundColor: '$gray800',
     borderRadius: '$md',
-    border: '1px solid $gray600',
+    border: '1px solid $luna600',
 })
 
 Box.displayName = 'Box'
