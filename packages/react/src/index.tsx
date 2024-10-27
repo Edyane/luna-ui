364ede2d9082dd07b7705b0ed832c213774b3fa5
Edyane <edyanealves@gmail.com>
2024-10-27 08:06:16 -0300

    feat: importando componentes

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,65 +1,12 @@
-import { ComponentProps } from 'react';
-import { styled } from './styles';
-
-export const Button = styled('button', {
-    fontFamily: '$default',
-    fontSize: '$sm',
-    fontWeight: '$medium',
-    backgroundColor: '$luna600',
-    borderRadius:'$xs',
-    padding: '$4 $6',
-    border: 0,
-    color: '$white',
-    width: 'auto',
-    cursor: 'pointer',
-    transition: 'background-color 0.3s ease, color 0.3s ease',
-    // '&:hover': {
-    //     backgroundColor: '#4545457a'
-    // },
-    
-    variants: {
-        type: {
-            second: {
-                backgroundColor: '$luna400',
-            },
-            tertiary: {
-                backgroundColor: '$white',
-                color: '$luna800',
-                border: '1px solid $luna800'
-            },
-            danger: {
-                backgroundColor: '$lunaRed',
-            },
-            success: {
-                backgroundColor: '$lunaGreen',
-            },
-            primaryRound: {
-                borderRadius:'$xg',
-            },
-            round: {
-                backgroundColor: '$luna100',
-                borderRadius:'$xg',
-                border: '1px solid $luna300',
-                color: '$luna500'
-            },
-        },
-        // size: {
-        //     large: {
-        //         fontSize: '$md',
-        //         padding: '$4 $10', 
-        //         fontWeight: '$bold',
-        //     },
-        //     small: {
-        //         fontSize: '$xs',
-        //         padding: '$3 $5',
-        //     },
-        // },
-    },
-    
-    //quando não for setado nenhum tipo específico
-    // defaultVariants: {
-    //     size: 'small',
-    // },
-});
-
-export type ButtonProps = ComponentProps<typeof Button>
+export * from '../src/components/Avatar';
+export * from './components/Box';
+export * from '../src/components/Button';
+export * from '../src/components/Checkbox';
+export * from '../src/components//Heading';
+export * from '../src/components/MultiStep';
+export * from '../src/components/Text';
+export * from '../src/components/TextArea';
+export * from '../src/components/TextInput';
+export * from '../src/components/Toast';
+export * from '../src/components/Tooltip';
+export * from '../src/styles';
