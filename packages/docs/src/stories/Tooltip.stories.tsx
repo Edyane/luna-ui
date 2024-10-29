93818ed8d79eb7aef636939382cbbc88698f57e9
Edyane <edyanealves@gmail.com>
2024-10-28 22:47:33 -0300

    fix: ajustando propriedades do tooltip

--- packages/docs/src/stories/Tooltip.stories.tsx
+++ packages/docs/src/stories/Tooltip.stories.tsx
@@ -6,14 +6,15 @@ import {
 	TooltipProps,
 	TooltipProvider,
 } from '@luna-ui/react';
+import './styles.css';
 
 export default {
 	title: 'Surfaces/Tooltip ',
 	component: Tooltip,
 	args: {
 		children: (
-			<Box css={{ width: '$6' }}>
-				<Text css={{ display: 'inline' }}>26</Text>
+			<Box css={{ width: '$12' }}>
+				<Text css={{ display: 'flex', justifyContent: 'center', cursor: 'pointer' }}>26</Text>
 			</Box>
 		),
 		description: 'October 26 - Available',
